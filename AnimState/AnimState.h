7d74d7a6a0598644cbef4021a613c7b18d654253
edowney <edowney@icct.com>
2018-03-08 19:28:32 -0500

    Initial Commit

---
+++ AnimState/AnimState.h
@@ -0,0 +1,19 @@
+//
+//  AnimState.h
+//  AnimState
+//
+//  Created by Downey, Eric on 3/8/18.
+//  Copyright © 2018 Downey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnimState.
+FOUNDATION_EXPORT double AnimStateVersionNumber;
+
+//! Project version string for AnimState.
+FOUNDATION_EXPORT const unsigned char AnimStateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnimState/PublicHeader.h>
+
+
